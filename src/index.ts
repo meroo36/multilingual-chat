e8f11db2449a7d581fa64415b430d05937ef40bb
Mert Doğruca <a7xlife@hotmail.com>
2021-12-04 20:34:48 +0300

    add livereload package to refresh the page otomatically

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,22 @@ import http from "http";
 const app = express();
 export const server = http.createServer(app);
 import "./socket/index";
+import livereload from "livereload";
+import path from "path";
+import connectLivereload from "connect-livereload";
+
+app.use(connectLivereload());
 dotenv.config();
 
 app.get("/", (req: Request, res: Response) => {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+app.get("/public/*", (req: Request, res: Response) => {
+    res.sendFile(__dirname + req.path);
+});
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
 
 server.listen(process.env.PORT, () => console.log(`Server listening on http://localhost:${process.env.PORT}`));
