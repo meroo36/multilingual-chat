c7c24fb796408348bd9079fe88e363e30d18bea2
Mert Doğruca <a7xlife@hotmail.com>
2021-12-04 20:34:07 +0300

    implement joining a room functionality

--- src/socket/roomListener.ts
+++ src/socket/roomListener.ts
@@ -1,6 +1,17 @@
 import { Socket } from "socket.io";
+import Rooms from "../model/room";
+import User from "../model/user";
 
-const joinRoom = (socket: Socket, roomId: string, username: string, rooms: Object) => {};
+const joinRoom = (socket: Socket, roomId: string, user: User, rooms: Rooms) => {
+    const room = rooms[roomId];
+    if (room) {
+        let socketId = socket.id;
+        user.socket_id = socketId;
+        room.users.push(user);
+        socket.join(roomId);
+        socket.to(roomId).emit("room:userJoined", user.username);
+    } else return;
+};
 const leaveRoom = () => {};
 
 export { joinRoom, leaveRoom };
