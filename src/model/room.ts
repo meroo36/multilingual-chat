a5d6dd96cdf707b4ba6efa66705879215754e851
Mert Doğruca <a7xlife@hotmail.com>
2021-12-04 20:35:16 +0300

    improve models

--- src/model/room.ts
+++ src/model/room.ts
@@ -4,6 +4,7 @@ import User from "./user";
 interface Rooms {
     [room_name: string]: {
         owner_socket_id: string;
+        only_owner_can_chat: boolean;
         users: User[];
         messages?: Message[];
     };
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,6 +1,7 @@
 interface User {
     lang: string;
     username: string;
-    socket_id: string;
+    socket_id?: string;
+    is_owner?: boolean;
 }
 export default User;
