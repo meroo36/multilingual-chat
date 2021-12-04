f4b6cbe92b70f401b82c1b7b0a7d3185ecb94c5e
Mert Doğruca <a7xlife@hotmail.com>
2021-12-04 20:33:27 +0300

    add socket listener for chat

--- src/socket/chatListener.ts
+++ src/socket/chatListener.ts
@@ -1,3 +1,13 @@
-const joinRoom = () => {};
+import { Socket } from "socket.io";
+import Message from "../model/message";
+import Rooms from "../model/room";
 
-export default joinRoom;
+const sendMessage = (socket: Socket, roomId: string, username: string, message: Message, rooms: Rooms) => {
+    const room = rooms[roomId];
+    if (room) {
+        //sending the translated message to corresponding users
+        //socket.to(roomId).emit("message:recieve", username, message);
+    }
+};
+
+export { sendMessage };
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,21 +1,27 @@
 import { server } from "..";
 import { Server, Socket } from "socket.io";
 import { joinRoom, leaveRoom } from "./roomListener";
+import { sendMessage } from "./chatListener";
 import Rooms from "../model/room";
+import User from "../model/user";
+import Message from "../model/message";
 
 const io = new Server(server);
 
 const rooms: Rooms = {
-    asdasd: {
-        owner_socket_id: "123213",
+    global_room: {
+        owner_socket_id: "999999",
+        only_owner_can_chat: false,
         messages: [],
         users: [],
     },
 };
 
 io.on("connection", (socket: Socket) => {
-    socket.on("room:join", (roomId, username) => joinRoom(socket, roomId, username, rooms));
+    socket.emit("rooms:get", rooms);
+    socket.on("room:join", (roomId: string, user: User) => joinRoom(socket, roomId, user, rooms));
 
+    socket.on("message:send", (roomId: string, username: string, message: Message) => sendMessage(socket, roomId, username, message, rooms));
     socket.on("disconnecting", leaveRoom);
     socket.on("disconnect", (data) => {
         console.log("Disconnected socket rooms ", socket.rooms);
