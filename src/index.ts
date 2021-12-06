a586f79e74d07e364b38b8f936618b30a7f727e7
Mert Doğruca <a7xlife@hotmail.com>
2021-12-06 15:29:24 +0300

    setup deploy

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,8 @@ attachSocketIo(server);
 app.get("/", (req: Request, res: Response) => {
     res.sendFile(__dirname + "/public/index.html");
 });
-
-if (process.env.NODE_ENV === "production") {
-    app.get("/app/dist/public/*", (req: Request, res: Response) => {
-        res.sendFile(__dirname + req.path);
-    });
-} else {
-    app.get("/public/*", (req: Request, res: Response) => {
-        res.sendFile(__dirname + req.path);
-    });
-}
+app.get("/public/*", (req: Request, res: Response) => {
+    res.sendFile(__dirname + req.path);
+});
 
 server.listen(process.env.PORT, () => console.log(`Server listening on http://localhost:${process.env.PORT}`));
