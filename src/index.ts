34d3a6be97f5776a92df3d0a4551186874b10f48
Mert Doğruca <a7xlife@hotmail.com>
2021-12-06 14:53:09 +0300

    remove livereload package

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import http from "http";
 const app = express();
-import livereload from "livereload";
 import path from "path";
-import connectLivereload from "connect-livereload";
 import attachSocketIo from "./socket";
 
-app.use(connectLivereload());
 dotenv.config();
 
 const server = http.createServer(app);
@@ -20,8 +17,5 @@ app.get("/", (req: Request, res: Response) => {
 app.get("/public/*", (req: Request, res: Response) => {
     res.sendFile(__dirname + req.path);
 });
-
-const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, "public"));
 
 server.listen(process.env.PORT, () => console.log(`Server listening on http://localhost:${process.env.PORT}`));
